35d457d6eb2707842473212b07b12219e9c4d8f2
petro2323 <aleksandarpetronijevic65@gmail.com>
2024-07-22 14:08:39 +0200

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+setInterval(setClock, 1000)
+
+const hour_hand = document.querySelector('[hour-hand]')
+const minute_hand = document.querySelector('[minute-hand]')
+const second_hand = document.querySelector('[second-hand]')
+
+function setClock() {
+    const current_date = new Date()
+
+    const seconds = current_date.getSeconds() / 60
+    const minutes = (seconds + current_date.getMinutes()) / 60
+    const hours = (minutes + current_date.getHours()) / 12
+
+    setRotation(second_hand, seconds)
+    setRotation(minute_hand, minutes)
+    setRotation(hour_hand, hours)
+}
+
+function setRotation(element, rotation) {
+    element.style.setProperty('--rotation', rotation * 360)
+}
+
+setClock()
